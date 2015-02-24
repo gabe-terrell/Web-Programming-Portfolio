ffe177498fc75a459081b2b4188c7860a7460c9c
gabe-terrell <Gabe.C.Terrell+GitHub@gmail.com>
2015-02-24 13:00:00 -0500

    Implemented Loading of Backdrop for Game
    I’m not fully sure it’s positioned correctly yet…

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,6 +1,14 @@
-var img = new Image();
-img.addEventListener("load", function() {
-  // execute drawImage statements here
-}, false);
-img.src = "pacman10-hp-sprite.png";
+function init() {
+	// Set up canvas
+	var canvas = document.getElementById('game_canvas')
+	var ctx = canvas.getContext('2d');
 
+	// Set up image
+	var img = new Image();
+	img.src = "pacman10-hp-sprite.png";
+
+	// Draw Pac-Woman
+	img.onload = function() {
+		ctx.drawImage(img, 319, 0, 795 - 329, 244 - 106, 0, 0, 795 - 329, 244 - 106);
+	}
+}
