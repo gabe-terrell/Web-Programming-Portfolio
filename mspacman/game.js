ba5bd45582897f79e633fe2fec13082b96f065a6
gabe-terrell <Gabe.C.Terrell+GitHub@gmail.com>
2015-02-24 13:28:50 -0500

    Corrected Pac-Woman Placement
    We hit it boys

--- mspacman/game.js
+++ mspacman/game.js
@@ -7,10 +7,11 @@ function init() {
 	var img = new Image();
 	img.src = "pacman10-hp-sprite.png";
 
-	// Draw Pac-Woman
+	// Draw Image
 	img.onload = function() {
+		// Draw Backdrop
 		ctx.drawImage(img, 319, 0, 466, 138, 400 - 233, 0, 795 - 329, 244 - 106);
-		ctx.drawImage(img, 80, 20, 15, 15, 400 - 180, 30, 15, 15)
+		// Draw Pac-Woman
+		ctx.drawImage(img, 80, 20, 15, 17, 400 - 197, 30, 15, 15)
 	}
 }
-122 - 102
