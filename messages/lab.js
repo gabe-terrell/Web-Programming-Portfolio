d789afabbeb7ea9181ee560b8de72d58f1c45041
gabe-terrell <Gabe.C.Terrell+GitHub@gmail.com>
2015-03-10 12:17:32 -0400

    Added JS Parsing to gh-pages
    Not entirely sure I understand gh-p, but we’ll see..

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,26 @@
-// Your JavaScript goes here...
+function parseData() {
+	// If request is succesful with correct status
+	if (request.readyState == 4 && request.status == 200) {
+		messagesDiv = document.getElementById("messages");
+		converted = JSON.parse(request.responseText);
+		for (i = 0; i < converted.length; i++) {
+			console.log(converted[i]['content']);
+			messagesDiv.innterHTML += "<p>" + converted[i]['content'] + "</p>";
+		}
+}
+
+function parse() {
+	// Step 1 - create instance of object
+	// If I prepend var to request, this will no longer work
+	// request becomes a local var, so parseData() gets very confused.
+	request = new XMLHttpRequest();
+
+	// Step 2 - create or "open" HTTP request
+	request.open("GET", "data.json", true);
+
+	// Step 3 - Set up way to manage response (with a function)
+	request.onreadystatechange = parseData;
+
+	// Step 4 - 
+	request.send();
+}
